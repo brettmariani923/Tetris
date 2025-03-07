99d088c1372c34b7e1a0a83cbd2784564440293c
Brett Mariani <brettmariani923@gmail.com>
2025-03-07 00:23:10 -0600

    added music

--- Program.cs
+++ Program.cs
@@ -6,15 +6,25 @@ using System.Threading;
 using System.Diagnostics.Metrics;
 using System.Threading.Tasks.Sources;
 using System.ComponentModel.Design.Serialization;
+using NAudio.Wave;
+using System.Diagnostics;
 
 
 namespace Tetris
 {
     public class Program
     {
-       
+
+        private static IWavePlayer waveOut;
+        private static AudioFileReader audioFile;
+
         public static void Main(string[] args)
         {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "3-32 Museum - Welcome to the Museum!.mp3");
+            PlayBackgroundMusic(filePath);
+
+            waveOut?.Stop();
+
         Start:
             string input;
             bool gameOver = false;
@@ -32,7 +42,7 @@ namespace Tetris
                     Console.Clear();
                     Console.WriteLine("G A M E  O V E R");
                     Console.WriteLine($"Score: {Grid.score}");
-                    Console.WriteLine("My Emperor... I've Failed Youuuuuu!!");
+                 
                     Console.WriteLine("www.youtube.com/watch?v=sDipbctxGC4");
                   
                    
@@ -62,5 +72,34 @@ namespace Tetris
             Console.SetCursorPosition(0, 0);
             goto Start;
         }
+        private static void PlayBackgroundMusic(string filePath)
+        {
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    Console.WriteLine("Error: MP3 file not found at " + filePath);
+                    return;
+                }
+
+                waveOut = new WaveOutEvent();
+                audioFile = new AudioFileReader(filePath);
+
+                // Loop the music when it stops
+                waveOut.PlaybackStopped += (s, e) =>
+                {
+                    audioFile.Position = 0; // Restart from the beginning
+                    waveOut.Play();
+                };
+
+                waveOut.Init(audioFile);
+                waveOut.Play();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error playing sound: {ex.Message}");
+            }
+        }
+
     }
 }
